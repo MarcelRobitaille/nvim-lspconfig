2b2905e55aee22c12a7b9501a1eabe853eb30736
Marcel Robitaille <marcel.robitaille@kuka.com>
2024-04-16 10:56:05 +0200

    WIP: Cspell support

---
+++ lua/lspconfig/server_configurations/cspell.lua
@@ -0,0 +1,30 @@
+local util = require 'lspconfig.util'
+
+local root_files = {
+  '.git',
+}
+
+return {
+  default_config = {
+    cmd = { 'node', '/home/kuka/code/vscode-spell-checker/packages/_server/dist/main.cjs', '--stdio' },
+    filetypes = { '*' },
+    root_dir = function(fname)
+      return util.root_pattern(unpack(root_files))(fname)
+    end,
+    single_file_support = true,
+    settings = {
+      cspell = {
+        analysis = {
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = true,
+          diagnosticMode = 'openFilesOnly',
+        },
+      },
+    },
+  },
+  commands = {
+  },
+  docs = {
+    description = [[]],
+  },
+}
